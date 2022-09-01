fb5f6b07594a44b8d97c84e5a08d7dbe3c335c5e
xysugino <xysugino@bu.edu>
2022-09-01 20:46:34 +0900

    made changes suggested

--- javascript.js
+++ javascript.js
@@ -1,64 +1,33 @@
-let ratingNumber = 0;
+"strict";
 
-document.querySelector(".circle-5").addEventListener("click", function () {
-  if (ratingNumber !== 0) {
-    //change the background color of the previous rated one back to normal;
-    document.querySelector(".circle-" + ratingNumber).style.backgroundColor =
-      "hsl(213, 19%, 18%)";
-  }
-  document.querySelector(".circle-5").style.backgroundColor =
-    " hsl(216, 12%, 54%)";
-  ratingNumber = 5;
-});
+//Initialize Variables
+const ratingOption = document.querySelectorAll(".circle");
+const submitButton = document.querySelector(".submit-button");
+const one = document.querySelector(".one");
+let userPick = -1;
 
-document.querySelector(".circle-4").addEventListener("click", function () {
-  if (ratingNumber !== 0) {
-    //change the background color of the previous rated one back to normal;
-    document.querySelector(".circle-" + ratingNumber).style.backgroundColor =
-      "hsl(213, 19%, 18%)";
-  }
-  document.querySelector(".circle-4").style.backgroundColor =
-    " hsl(216, 12%, 54%)";
-  ratingNumber = 4;
-});
+//Handles user selector changes
+for (let i = 0; i < ratingOption.length; i++) {
+  ratingOption[i].addEventListener("click", function () {
+    submitButton.disabled = false;
+    //case when rating is already selected
+    if (userPick !== -1) {
+      ratingOption[userPick].classList.remove("button-selected");
+      //ratingOption[userPick].style.backgroundColor = "hsl(214, 16%, 25%)";
+      //ratingOption[userPick].style.color = "hsl(217, 12%, 63%";
+    }
+    ratingOption[i].classList.add("button-selected");
+    //ratingOption[i].style.backgroundColor = "hsl(217, 12%, 63%)";
+    //ratingOption[i].style.color = "white";
+    userPick = i;
+  });
+}
 
-document.querySelector(".circle-3").addEventListener("click", function () {
-  if (ratingNumber !== 0) {
-    //change the background color of the previous rated one back to normal;
-    document.querySelector(".circle-" + ratingNumber).style.backgroundColor =
-      "hsl(213, 19%, 18%)";
-  }
-  document.querySelector(".circle-3").style.backgroundColor =
-    " hsl(216, 12%, 54%)";
-  ratingNumber = 3;
-});
-
-document.querySelector(".circle-2").addEventListener("click", function () {
-  if (ratingNumber !== 0) {
-    //change the background color of the previous rated one back to normal;
-    document.querySelector(".circle-" + ratingNumber).style.backgroundColor =
-      "hsl(213, 19%, 18%)";
-  }
-  document.querySelector(".circle-2").style.backgroundColor =
-    " hsl(216, 12%, 54%)";
-  ratingNumber = 2;
-});
-
-document.querySelector(".circle-1").addEventListener("click", function () {
-  if (ratingNumber !== 0) {
-    //change the background color of the previous rated one back to normal;
-    document.querySelector(".circle-" + ratingNumber).style.backgroundColor =
-      "hsl(213, 19%, 18%)";
-  }
-  document.querySelector(".circle-1").style.backgroundColor =
-    " hsl(216, 12%, 54%)";
-  ratingNumber = 1;
-});
-
-document.querySelector(".submit-button").addEventListener("click", function () {
+//Handles change of state (from rating to thank you state)
+submitButton.addEventListener("click", function () {
   document.querySelector(".rating-state").style.display = "none";
   document.querySelector(".thank-you-state").style.display = "block";
-  document.querySelector(
-    ".select-message"
-  ).textContent = `You selected ${ratingNumber} out of 5`;
+  document.querySelector(".select-message").textContent = `You selected ${
+    userPick + 1
+  } out of 5`;
 });
